a62caebf3e37eab0fb4fd77d9d80f7b90ea9bd0b
GabVit <gabriel.n.vitelli@gmail.com>
2023-12-04 17:05:37 -0300

    Civil version1
    Se creo un script y un prefab para el civil y para el CivilSpawn en base a lo realizado para la IA del enemigo.
    Se debe continuar con la correccion de animaciones para que no haga movimientos como si tuviese un arma en la mano cuando se mueve o esta en cubierto.
    También se debe escribir un sistema de penalización al destruir la entidad por un disparo

---
+++ Assets/Scripts/CivilAI.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CivilAI : MonoBehaviour, ITakeDamage
+{
+    const string RUN_TRIGGER = "Run";
+    const string CROUCH_TRIGGER = "Crouch";
+
+    [SerializeField] private float startingHealth;
+    [SerializeField] private float minTimeUnderCover;
+    [SerializeField] private float maxTimeUnderCover;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float damage;
+    [Range(0, 100)]
+    [SerializeField] private ParticleSystem bloodSplatterFX;
+
+    private NavMeshAgent agent;
+    private Player player;
+    private Transform occupiedCoverSpot;
+    private Animator animator;
+    private float _health;
+
+    public float health
+    {
+        get { return _health; }
+        set { _health = Mathf.Clamp(value, 0, startingHealth); }
+    }
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        _health = startingHealth;
+        // Otras inicializaciones si es necesario
+        animator.SetTrigger(RUN_TRIGGER);
+    }
+
+    public void Init(Player player, Transform coverSpot)
+    {
+        occupiedCoverSpot = coverSpot;
+        this.player = player;
+        GetToCover();
+    }
+
+    private void GetToCover()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(occupiedCoverSpot.position);
+    }
+
+    private void Update()
+    {
+        RotateTowardsPlayer();
+
+        if (agent.isStopped == false && (transform.position - occupiedCoverSpot.position).sqrMagnitude <= 0.1f)
+        {
+            agent.isStopped = true;
+            HideBehindCover();
+        }
+    }
+
+    private void HideBehindCover()
+    {
+        animator.SetTrigger(CROUCH_TRIGGER);
+        StartCoroutine(StayUnderCover());
+    }
+
+    private IEnumerator StayUnderCover()
+    {
+        float timeUnderCover = Random.Range(minTimeUnderCover, maxTimeUnderCover);
+        yield return new WaitForSeconds(timeUnderCover);
+        // Puedes agregar lógica adicional aquí si es necesario
+        GetToCover();
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        Vector3 direction = player.GetHeadPosition() - transform.position;
+        direction.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        transform.rotation = rotation;
+    }
+
+    public void TakeDamage(Weapon weapon, Projectile projectile, Vector3 contactPoint)
+    {
+        health -= weapon.GetDamage();
+        Debug.Log("Civil Health: " + health);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("Civil destroyed!");
+        }
+
+        ParticleSystem effect = Instantiate(bloodSplatterFX, contactPoint, Quaternion.LookRotation(weapon.transform.position - contactPoint));
+        effect.Stop();
+        effect.Play();
+    }
+}
---
+++ Assets/Scripts/CivilSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CivilSpawner : MonoBehaviour
+{
+	[SerializeField] private Transform[] spawnPoints;
+	[SerializeField] private CivilAI civilPrefab;
+	[SerializeField] private float spawnInterval;
+	[SerializeField] private int maxCivilNumber;
+	[SerializeField] private Player player;
+
+	private List<CivilAI> spawnedCivil = new List<CivilAI>();
+	private float timeSinceLastSpawn;
+
+	private void Start()
+	{
+		timeSinceLastSpawn = spawnInterval;
+	}
+
+	private void Update()
+	{
+		timeSinceLastSpawn += Time.deltaTime;
+		if(timeSinceLastSpawn > spawnInterval)
+		{
+			timeSinceLastSpawn = 0f;
+			if(spawnedCivil.Count < maxCivilNumber)
+			{
+				SpawnCivil();
+			}
+		}
+	}
+
+	private void SpawnCivil()
+	{
+		CivilAI civil = Instantiate(civilPrefab, transform.position, transform.rotation);
+		int spawnPointindex = spawnedCivil.Count % spawnPoints.Length;
+		civil.Init(player, spawnPoints[spawnPointindex]);
+		spawnedCivil.Add(civil);
+	}
+
+	
+}
